71a26618566dc0fa303d872ac10c9642eca86353
Gokul Karthick <gokulkarthickct@gmail.com>
2016-10-03 20:53:40 +0530

    Add files via upload

---
+++ tweetledee/tldlib/keys/tweetledee_keys.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+* PLEASE READ THE FOLLOWING
+* Enter your Twitter application information in the appropriate lines below this comment section.
+* Please leave the single quotes around the alphanumeric strings that you enter.
+*
+*  An appropriately formatted keys file should look like this:
+*  	$my_consumer_key        = 'CoMpLetEly8oGusK3y';
+*	$my_consumer_secret     = 'SuPercaliFragilist1cEXPiaLidOCIous';
+*	$my_access_token        = 'AcC355MePLz';
+*	$my_access_token_secret = '5555shTh1s1iS53cr3t';
+*	$my_domain              = 'http://www.toolegittoquit.com';
+*
+*   Please note that the number of characters in the above examples are not necessarily correct and that these are bogus tokens.
+*   Users who have attempted a 'workaround' for their API limits received tweets from Mary Poppins and MC Hammer...
+*
+*  OK, now it's your turn.  I used ALL CAPS where you should insert your information.  If it's not in ALL CAPS, it's a good bet
+*  that you should not modify it.
+*
+*  Enter your information below this line:
+*/
+
+$my_consumer_key        = 'OjE5SRmLfYtZ7AwSrSCHtD3wY';
+$my_consumer_secret     = 'FktrYsprR45BcrXy2EYfBxKXFnBxflLVovemy5eKKMQlCh07vy';
+$my_access_token        = '271942159-rnCdRYV1btkPJv8VQKCHA2WOfu3nEh0L8tqh9HbZ';
+$my_access_token_secret = 'AgVcVcnBF2Z3RGZeWb4Ut5sdoc19THU8nBHQysO4NHSFS';
+$my_domain              = 'http://gdgcbe.com/';
+
+
+?>
